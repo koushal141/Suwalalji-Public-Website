7b5231bb53dcb28d073d55cf73752543cfc17af9
Kaushal19 <koushal141@gmail.com>
2022-03-08 13:58:16 +0530

    Imgur Images

---
+++ data/Images.js
@@ -0,0 +1,12 @@
+const Images = {
+  Orange180: "https://i.imgur.com/4nOv0ap.png",
+  Orange400: "https://i.imgur.com/W85cNL0.png",
+  Yellow400: "https://i.imgur.com/vxFsvnl.png",
+  Blue180: "https://i.imgur.com/ExOQUkt.png",
+  Blue400: "https://i.imgur.com/qU81R6Y.png",
+  Green180: "https://i.imgur.com/tnpyeZR.png",
+  Green400: "https://i.imgur.com/Q1ecWfe.png",
+  Brown400: "https://i.imgur.com/NVsDHu6.png",
+};
+
+export default Images;
